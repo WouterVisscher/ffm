040158b2309736ec980c1311964a2f7f67b33587
Wouter Visscher <wouter.visscher@gmail.com>
2024-04-03 22:59:33 +0200

    update proj_info code

--- src/main/java/nl/kadaster/ffm/Projversion.java
+++ src/main/java/nl/kadaster/ffm/Projversion.java
@@ -1,9 +1,7 @@
 package nl.kadaster.ffm;
 
 import static java.lang.foreign.ValueLayout.JAVA_CHAR;
-import static java.lang.foreign.ValueLayout.JAVA_CHAR_UNALIGNED;
 import static java.lang.foreign.ValueLayout.JAVA_INT;
-import static java.lang.foreign.ValueLayout.ADDRESS;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.FunctionDescriptor;
@@ -14,50 +12,68 @@ import java.lang.foreign.MemorySegment;
 import java.lang.foreign.SegmentAllocator;
 import java.lang.foreign.StructLayout;
 import java.lang.foreign.SymbolLookup;
-import java.lang.foreign.ValueLayout;
 import java.lang.invoke.VarHandle;
 
-
 public class Projversion {
+
+        // {
+        // major = 9,
+        // minor = 3,
+        // patch = 1,
+        // release = 0x7f361a4ddf30 <pj_release> "Rel. 9.3.1, December 1st, 2023",
+        // version = 0x7f361a5a05a0 <version> "9.3.1",
+        // searchpath = 0x7f364c2976b0
+        // "/home/wouter/.local/share/proj:/usr/local/share/proj:/usr/local/share/proj",
+        // paths = 0x0,
+        // path_count = 0
+        // }
         public static void main(String[] args) throws Throwable {
 
                 StructLayout T_Version = MemoryLayout.structLayout(
                                 JAVA_INT.withName("major"),
                                 JAVA_INT.withName("minor"),
-                                JAVA_INT.withName("patch"),                        
+                                JAVA_INT.withName("patch"),
                                 JAVA_CHAR.withName("release"),
                                 JAVA_CHAR.withName("version"),
                                 JAVA_CHAR.withName("searchpath"),
                                 JAVA_CHAR.withName("paths"),
                                 JAVA_INT.withName("path_count"));
 
-                System.load("/home/wouter/work/repo/github.com/WouterVisscher/ffm/src/main/c/libprojversion.so");
-                // System.load("/usr/local/lib/libproj.so");
-                
+                // System.load("/home/wouter/work/repo/github.com/WouterVisscher/ffm/src/main/c/libprojversion.so");
+                System.load("/usr/local/lib/libproj.so");
+
                 SymbolLookup symbolLookup = SymbolLookup.loaderLookup();
 
-                final var versionSymbol = symbolLookup.find("get_static_proj_info")
+                final var versionSymbol = symbolLookup.find("proj_info")
                                 .orElseThrow(() -> new Exception("Could not find get_static_proj_info"));
 
                 final var versionSig = FunctionDescriptor.of(T_Version);
-                final var version = Linker.nativeLinker().downcallHandle(versionSymbol, versionSig);
+                final var proj_version = Linker.nativeLinker().downcallHandle(versionSymbol, versionSig);
 
                 try (Arena arena = Arena.ofConfined()) {
-                        SegmentAllocator allocator = SegmentAllocator.slicingAllocator(arena.allocate(24));
+                        SegmentAllocator allocator = SegmentAllocator.slicingAllocator(arena.allocate(100));
 
-                        MemorySegment result = (MemorySegment) version.invoke(allocator);
+                        MemorySegment result = (MemorySegment) proj_version.invokeExact(allocator);
                         System.out.println(result);
                         result = result.reinterpret(T_Version.byteSize());
-                        
+
                         VarHandle major = T_Version.varHandle(PathElement.groupElement("major"));
                         VarHandle minor = T_Version.varHandle(PathElement.groupElement("minor"));
                         VarHandle patch = T_Version.varHandle(PathElement.groupElement("patch"));
-                        // VarHandle paths = T_Version.varHandle(PathElement.groupElement("paths"));
+                        VarHandle release = T_Version.varHandle(PathElement.groupElement("release"));
+                        VarHandle version = T_Version.varHandle(PathElement.groupElement("version"));
+                        VarHandle searchpath = T_Version.varHandle(PathElement.groupElement("searchpath"));
+                        VarHandle paths = T_Version.varHandle(PathElement.groupElement("paths"));
+                        VarHandle path_count = T_Version.varHandle(PathElement.groupElement("path_count"));
 
                         System.out.println(major.get(result));
                         System.out.println(minor.get(result));
                         System.out.println(patch.get(result));
-                        // System.out.println(patch.get(paths));
+                        System.out.println(release.get(result));
+                        System.out.println(version.get(result));
+                        System.out.println(searchpath.get(result));
+                        System.out.println(paths.get(result));
+                        System.out.println(path_count.get(result));
                 }
         }
 }
