a28c8fb4a6a4541eefe0ee1ffe631115620dec7a
Wouter Visscher <wouter.visscher@gmail.com>
2024-04-07 22:10:47 +0200

    helper func

--- src/main/java/nl/kadaster/ffm/Projversion.java
+++ src/main/java/nl/kadaster/ffm/Projversion.java
@@ -1,10 +1,12 @@
 package nl.kadaster.ffm;
 
+import static java.lang.foreign.ValueLayout.JAVA_BYTE;
 import static java.lang.foreign.ValueLayout.JAVA_CHAR;
 import static java.lang.foreign.ValueLayout.JAVA_INT;
 
 import java.lang.foreign.Arena;
 import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.GroupLayout;
 import java.lang.foreign.Linker;
 import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemoryLayout.PathElement;
@@ -12,7 +14,9 @@ import java.lang.foreign.MemorySegment;
 import java.lang.foreign.SegmentAllocator;
 import java.lang.foreign.StructLayout;
 import java.lang.foreign.SymbolLookup;
+import java.lang.invoke.MethodHandle;
 import java.lang.invoke.VarHandle;
+import java.nio.charset.StandardCharsets;
 
 public class Projversion {
 
@@ -33,11 +37,11 @@ public class Projversion {
                                 JAVA_INT.withName("major"),
                                 JAVA_INT.withName("minor"),
                                 JAVA_INT.withName("patch"),
-                                JAVA_CHAR.withName("release"),
+                                MemoryLayout.sequenceLayout(32, JAVA_BYTE).withName("release"),
                                 JAVA_CHAR.withName("version"),
                                 JAVA_CHAR.withName("searchpath"),
                                 JAVA_CHAR.withName("paths"),
-                                JAVA_INT.withName("path_count"));
+                                JAVA_CHAR.withName("path_count")).withName("PJ_INFO");
 
                 // System.load("/home/wouter/work/repo/github.com/WouterVisscher/ffm/src/main/c/libprojversion.so");
                 System.load("/usr/local/lib/libproj.so");
@@ -51,7 +55,7 @@ public class Projversion {
                 final var proj_version = Linker.nativeLinker().downcallHandle(versionSymbol, versionSig);
 
                 try (Arena arena = Arena.ofConfined()) {
-                        SegmentAllocator allocator = SegmentAllocator.slicingAllocator(arena.allocate(100));
+                        SegmentAllocator allocator = SegmentAllocator.slicingAllocator(arena.allocate(200));
 
                         MemorySegment result = (MemorySegment) proj_version.invokeExact(allocator);
                         System.out.println(result);
@@ -60,7 +64,7 @@ public class Projversion {
                         VarHandle major = T_Version.varHandle(PathElement.groupElement("major"));
                         VarHandle minor = T_Version.varHandle(PathElement.groupElement("minor"));
                         VarHandle patch = T_Version.varHandle(PathElement.groupElement("patch"));
-                        VarHandle release = T_Version.varHandle(PathElement.groupElement("release"));
+                        // VarHandle release = T_Version.varHandle(PathElement.groupElement("release"));
                         VarHandle version = T_Version.varHandle(PathElement.groupElement("version"));
                         VarHandle searchpath = T_Version.varHandle(PathElement.groupElement("searchpath"));
                         VarHandle paths = T_Version.varHandle(PathElement.groupElement("paths"));
@@ -69,11 +73,18 @@ public class Projversion {
                         System.out.println(major.get(result));
                         System.out.println(minor.get(result));
                         System.out.println(patch.get(result));
-                        System.out.println(release.get(result));
+                        System.out.println(getFixedString(result, T_Version, "release" ));
                         System.out.println(version.get(result));
                         System.out.println(searchpath.get(result));
                         System.out.println(paths.get(result));
                         System.out.println(path_count.get(result));
                 }
         }
+
+        public static String getFixedString(MemorySegment memorySegment, GroupLayout groupLayout, String name) throws Throwable {
+                MethodHandle methodHandle = groupLayout.sliceHandle(MemoryLayout.PathElement.groupElement(name));
+                MemorySegment namedMemorySegment = (MemorySegment) methodHandle.invokeExact(memorySegment);
+                byte[] namedData = namedMemorySegment.toArray(JAVA_BYTE);
+                return new String((new String(namedData)).getBytes(StandardCharsets.UTF_16BE));
+            }
 }
